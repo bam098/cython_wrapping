a754bafde7b8614e2f883851a11f9da8c42577de
bam098 <bam00980@gmail.com>
2020-07-02 04:23:53 +0200

    make code work under linux

--- 06_wrapper_package/setup.py
+++ 06_wrapper_package/setup.py
@@ -1,6 +1,21 @@
 from setuptools import setup, find_packages
-from setuptools.extension import Extension
+from distutils.extension import Extension
+import distutils.sysconfig
 from Cython.Build import cythonize
+from sys import platform
+
+compile_args = ["-std=c++11"]
+link_args = []
+
+if platform == "linux":
+    cfg_vars = distutils.sysconfig.get_config_vars()
+    for key, value in cfg_vars.items():
+        if type(value) == str:
+            cfg_vars[key] = value.replace("-Wstrict-prototypes", "")
+elif platform == "darwin":
+    compile_args.append("-mmacosx-version-min=10.9")
+    link_args.append("-mmacosx-version-min=10.9")
+    link_args.append("-stdlib=libc++")
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -12,8 +27,8 @@ pyvector_extension = Extension(
     library_dirs=["pymath/lib"],
     include_dirs=["pymath/lib"],
     language="c++",
-    extra_compile_args=['-std=c++11', "-mmacosx-version-min=10.9"],
-    extra_link_args=["-stdlib=libc++", "-mmacosx-version-min=10.9"],
+    extra_compile_args=compile_args,
+    extra_link_args=link_args,
 )
 
 setup(
